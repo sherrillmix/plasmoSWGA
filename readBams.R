7419331538cf90c0ca61f6abc180fa9dae17781d
Scott Sherrill-Mix <sherrillmix@users.noreply.github.com>
2016-05-18 18:48:34 -0400

    Basic plotting

--- readBams.R
+++ readBams.R
@@ -13,6 +13,9 @@ for(ii in bamFiles){
 		cmd<-sprintf('samtools sort -@ 6 -m 8G %s %s',ii,sortPrefix)
 		message(cmd)
 		system(cmd)
+		cmd<-sprintf('samtools index %s',sortFile)
+		message(cmd)
+		system(cmd)
 	}
 	if(!file.exists(outFile)){
 		cmd<-sprintf('~/installs/bedCount/bam2depth %s>%s',sortFile,outFile)
@@ -34,6 +37,25 @@ predictCover<-mclapply(primerSets,function(primers){
 	rs<-lapply(rs,function(r)r+attr(r,'match.length')-1)
 	out<-mcmapply(predictAmplifications,fs,rs,SIMPLIFY=FALSE,MoreArgs=list(maxLength=30000),mc.cores=length(primers))
 	names(out)<-ref$name
-	return(out)
+	return(list('pred'=out,'for'=fs,'rev'=rs))
 })
 
+for(ii in names(predictCover)){
+	message(ii)
+	outFile<-file.path('out',sprintf('%s.png',ii))
+	thisCovers<-cover[grep(sprintf('data/%s',ii),names(cover))]
+	thisPredicts<-predictCover[[ii]][['pred']]
+	png(outFile,width=3000,height=3000,res=250)
+	par(mfrow=c(4,4))
+	ylim<-c(0,max(sapply(thisCovers,function(xx)max(xx$count))))
+	maxPredict<-max(sapply(thisPredicts,function(xx)max(xx$amplifications)))
+	for(jj in names(thisPredicts)){
+		thisCover<-lapply(thisCovers,function(x)x[x$chr==jj,])
+		thisPredict<-thisPredicts[[jj]]
+		plot(1,1,xlim=c(1,max(thisPredict$end)),ylim=ylim,main=jj,xlab='Position',ylab='Cover')
+		sapply(thisCover,function(xx)points(xx$pos,xx$count))
+		segments(thisPredict$start,thisPredict$amplifications/maxPredict*ylim[2],thisPredict$end,thisPredict$amplifications/maxPredict*ylim[2],col='#FF000099')
+		segments(thisPredict$end[-nrow(thisPredict)],thisPredict$amplifications[-nrow(thisPredict)]/maxPredict*ylim[2],thisPredict$start[-1],thisPredict$amplifications[-1]/maxPredict*ylim[2],col='#FF000099')
+	}
+	dev.off()
+}
